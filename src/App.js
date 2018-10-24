36ba7af1c83ebe9039d04a429206b4d3e44d10d3
HerbertLiskin <kljatsko-g.a@outlook.com>
2018-10-24 20:32:42 +0300

    add AppBar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import {
   Link,
 } from 'react-router-dom'
 
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
 import './styles/reset.scss'
 import styles from './styles/styles.scss'
 
 import Routes from './Routes'
 
+
 const App = () => (
     <BrowserRouter>
         <main>
-            <div>
-                <p>Admin app test</p>
-            </div>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton 
+                        color="inherit" 
+                        aria-label="Menu"
+                        className={styles.menuIcon}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        Admin app test
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <ul>
                 <li><Link to="/">Root</Link></li>
             </ul>
